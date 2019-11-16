c948d6494f8c7dae58b9593921f2ea0ea84b2348
Ashan Shanaka <ashansjp@gmail.com>
2019-11-16 15:12:06 +0530

    login is ok

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const User = mongoose.model('User');
+require('../models/user.model');
 
+
+const User = mongoose.model('User');
 let router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('login/userLogin');
 });
+
+router.post('/', (req, res) => {
+    console.log(req.body.email);
+    User.findOne({email: req.body.email}).then(user => {
+        if(user){
+            res.redirect('/employee/list');
+            // console.log('user is available');
+        }else{
+            res.redirect('/employee');
+        }
+    }).catch( err => {
+        console.log(err);
+        return;
+    });
+})
 
 module.exports = router;
